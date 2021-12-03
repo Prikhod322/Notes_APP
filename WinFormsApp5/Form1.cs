dd4acceaa4eec5611cf91c0f6158c3ec895efb0d
Prikhod322 <vovkaprikhod@gmail.com>
2021-12-04 01:41:42 +0200

    Добавьте файлы проекта.

---
+++ WinFormsApp5/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+            listBox1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            textBox1.Visible = false;
+            textBox4.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2($"{textBox2.Text}|{textBox3.Text}|{(checkBox1.Checked==true?"True":"False")}");
+            f2.ShowDialog();
+        }
+
+        int SelectedIDX;
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                textBox1.Text = texts[listBox1.SelectedIndex];
+                textBox4.Text = dates[listBox1.SelectedIndex];
+                SelectedIDX = listBox1.SelectedIndex;
+            }
+            else
+            {
+                textBox1.Text = string.Empty;
+            }
+        }
+
+        private void Show()
+        {
+                connection.Open();
+            if (deleteMode == false)
+            {
+                oString = "Select * FROM [NOTE]";
+                oCmd = new SqlCommand(oString, connection);
+                oReader = oCmd.ExecuteReader();
+                listBox1.Items.Clear();
+                texts.Clear();
+                while (oReader.Read())
+                {
+                    listBox1.Items.Add(oReader["NAME"]);
+                    texts.Add(oReader["TEXT"].ToString());
+                    dates.Add(oReader["CREATION_TIME"].ToString());
+                }
+                
+                if(listBox1.Items.Count>0)
+                listBox1.SelectedIndex = SelectedIDX;
+            }
+            else
+            {
+                //oString = $"Delete FROM [NOTE] WHERE ROW_NUMBER={listBox1.SelectedIndex + 1}";
+                oString = $"Delete [NOTE] * From( Select Row_Number() Over(Order By ID) As RowNum, * From [NOTE]) [NOTE] Where RowNum = {listBox1.SelectedIndex + 1}";
+                oCmd = new SqlCommand(oString, connection);
+                oReader = oCmd.ExecuteReader();
+
+               // MessageBox.Show($"Delete FROM [NOTE] WHERE ROW_NUMBER={listBox1.SelectedIndex + 1}");
+
+                deleteMode = false;
+            }
+                connection.Close();
+        }
+
+
+        private async void Update()
+        {
+
+            Action action = () =>
+            {
+                Show();
+            };
+
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await Task.Delay(500);
+                    Invoke(action);
+                }
+            });
+        }
+        SqlConnection connection;
+        string connectionString;
+        string oString;
+        SqlCommand oCmd;
+        SqlDataReader oReader;
+        List<string> texts = new List<string>();
+        List<string> dates = new List<string>();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            connectionString = $"Data Source={textBox2.Text};Initial Catalog={textBox3.Text};Integrated Security=True;";
+            connection = new SqlConnection(connectionString);
+            listBox1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            textBox1.Visible = true;
+            textBox4.Visible = true;
+            Update();
+        }
+        bool deleteMode = false;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            deleteMode = true;
+        }
+    }
+}
---
+++ WinFormsApp5/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    public partial class Form2 : Form
+    {
+        string DataSourse;
+        string InitialCatalog;
+        string IntegratedSecurity;
+        public Form2(string connection)
+        {
+            InitializeComponent();
+
+            DataSourse = connection.Split('|')[0];
+            InitialCatalog = connection.Split('|')[1];
+            IntegratedSecurity = connection.Split('|')[2];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection($"Data Source={DataSourse};Initial Catalog={InitialCatalog};Integrated Security={IntegratedSecurity};"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(@$"INSERT INTO NOTE(NAME,TEXT,CREATION_TIME) VALUES('{textBox1.Text}','{textBox2.Text}','{DateTime.Now}')", connection))
+                {
+                    int rows = command.ExecuteNonQuery();
+                    if (rows > 0)
+                        MessageBox.Show("Note Added!");
+                    else
+                        Console.WriteLine("ERROR!");
+
+                }
+                connection.Close();
+                    this.Close();
+            }
+        }
+    }
+}
